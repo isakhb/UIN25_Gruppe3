db180b8c0fe86aaed540104242e51c08a9ac110b
isakhb <143383625+isakhb@users.noreply.github.com>
2025-05-12 14:42:28 +0200

    Mappestruktur + installert react-router-dom

---
+++ Billettlyst/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import "../styles/header.scss";
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    {/*
+                    <li><Link to="/">Hjem</Link></li>
+                    <li><Link to="/category/:slug">Musikk</Link></li>
+                    <li><Link to="/category/:slug">Sport</Link></li>
+                    <li><Link to="/category/:slug">Teater</Link></li>
+                    <li><Link to="/dashboard">Logg inn</Link></li>
+                    */}
+                </ul>
+            </nav>
+        </header>
+    )
+}
