828c00dfcb40a6ebbbe9038d94bec59031afd67f
Nils2312 <143592488+Nils2312@users.noreply.github.com>
2025-05-16 22:21:01 +0200

    Delete Billettlyst directory

--- Billettlyst/src/components/CategoryPage.jsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from "react-router-dom"
-
-export default function CategoryPage() {
-    const { slug } = useParams()
-    const categoryOverskrift = slug.charAt(0).toUpperCase() + slug.slice(1) 
-    return (
-        <h2>{categoryOverskrift}</h2>
-    )
-}
