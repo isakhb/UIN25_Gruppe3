8edba09b9cbddbcc7fb8bd645320064835f9871c
Nils2312 <143592488+Nils2312@users.noreply.github.com>
2025-05-18 16:59:08 +0200

    Oppdaterte EventCard.jsx til å vise riktig navn

--- Billettlyst/src/components/EventCard.jsx
+++ Billettlyst/src/components/EventCard.jsx
@@ -4,6 +4,13 @@ export default function EventCard(props) {
   const event = props.event;
   const clickable = props.clickable;
 
+  let visningsnavn;
+  if (clickable) {
+    visningsnavn = event._embedded.attractions[0].name;
+  } else {
+    visningsnavn = event.name;
+  }
+
   return (
     <article className="festivalkort">
       <img
@@ -11,21 +18,21 @@ export default function EventCard(props) {
         alt={event.name}
         className="festivalbilde"
       />
-      <h3>{event.name}</h3>
+      <h3>{visningsnavn}</h3>
 
-      {!clickable && (
+      {clickable === false && (
         <ul className="eventinfo">
           <li>Dato: {event.dates.start.localDate}</li>
-          <li>Tid: {event.dates.start.localTime || "Ikke oppgitt"}</li>
+          <li>Tid: {event.dates.start.localTime ? event.dates.start.localTime : "Ikke oppgitt"}</li>
           <li>Land: {event._embedded.venues[0].country.name}</li>
           <li>By: {event._embedded.venues[0].city.name}</li>
           <li>Scene: {event._embedded.venues[0].name}</li>
         </ul>
       )}
 
-      {clickable && (
+      {clickable === true && (
         <Link
-          to={`/event/${event._embedded.attractions[0].id}`}
+          to={"/event/" + event._embedded.attractions[0].id}
           className="festivalknapp"
         >
           Les mer om {event._embedded.attractions[0].name}
