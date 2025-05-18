de00d637dcfcb53b2b9220139f3dc970cfe502d7
Nils2312 <143592488+Nils2312@users.noreply.github.com>
2025-05-18 16:33:30 +0200

    Stylet Dashboard.jsx

--- Billettlyst/src/components/Dashboard.jsx
+++ Billettlyst/src/components/Dashboard.jsx
@@ -1,15 +1,16 @@
 export default function Dashboard() {
     return (
-        <section>
-            <h1>Dashboard</h1>
-            <form>
-                <h2>Logg inn</h2>
-                <label>
-                    Brukernavn:
-                    <input type="text" placeholder="brukernavn"/>
-                </label>
-                <input type="submit" value="Logg inn"/>
-            </form>
-        </section>
-    )
-}
+      <main className="dashboard">
+        <h1>Dashboard</h1>
+        <form className="loginform">
+          <h3>Logg inn</h3>
+  
+          <label htmlFor="username">Brukernavn:</label>
+          <input id="username" type="text" placeholder="brukernavn" />
+  
+          <input type="submit" value="Logg inn" />
+        </form>
+      </main>
+    );
+  }
+  
