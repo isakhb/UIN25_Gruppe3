47cd07a4ef789fc0afcf219235de0574f5abe6e8
Nils2312 <143592488+Nils2312@users.noreply.github.com>
2025-05-18 18:49:34 +0200

    La til PassCard

---
+++ Billettlyst/src/components/PassCard.jsx
@@ -0,0 +1,18 @@
+export default function PassCard({ festivalPass }) {
+  return (
+    <article className="festivalkort">
+      <img
+        src={festivalPass.images[0].url}
+        alt={festivalPass.name}
+        className="festivalbilde"
+      />
+      <h3>{festivalPass.name}</h3>
+      <h4>{festivalPass._embedded.venues[0].name}</h4>
+      <h4>{festivalPass._embedded.venues[0].city.name}</h4>
+      <h4>{festivalPass._embedded.venues[0].country.name}</h4>
+      <h4>{festivalPass.dates.start.localDate}</h4>
+      <button className="festivalknapp">Kjøp</button>
+      <button className="festivalknapp">Legg til ønskeliste</button>
+    </article>
+  );
+}
