ae144174914c8486a76b8ff0f567806cf608da68
Nils2312 <143592488+Nils2312@users.noreply.github.com>
2025-05-18 16:47:57 +0200

    Satte opp EventCard.jsx og koblet det til Home

--- Billettlyst/src/components/EventCard.jsx
+++ Billettlyst/src/components/EventCard.jsx
@@ -1,12 +1,36 @@
-export default function EventCard() {
-    return (
-        <article className="event-card">
-            <h3>Event tittel</h3>
-            <p>Dato</p>
-            <p>Klokke</p>
-            <p>Land</p>
-            <p>By</p>
-            <p>Sted</p>
-        </article>
-    )
+import { Link } from "react-router-dom";
+
+export default function EventCard(props) {
+  const event = props.event;
+  const clickable = props.clickable;
+
+  return (
+    <article className="festivalkort">
+      <img
+        src={event.images[0].url}
+        alt={event.name}
+        className="festivalbilde"
+      />
+      <h3>{event.name}</h3>
+
+      {!clickable && (
+        <ul className="eventinfo">
+          <li>Dato: {event.dates.start.localDate}</li>
+          <li>Tid: {event.dates.start.localTime || "Ikke oppgitt"}</li>
+          <li>Land: {event._embedded.venues[0].country.name}</li>
+          <li>By: {event._embedded.venues[0].city.name}</li>
+          <li>Scene: {event._embedded.venues[0].name}</li>
+        </ul>
+      )}
+
+      {clickable && (
+        <Link
+          to={`/event/${event._embedded.attractions[0].id}`}
+          className="festivalknapp"
+        >
+          Les mer om {event._embedded.attractions[0].name}
+        </Link>
+      )}
+    </article>
+  );
 }
