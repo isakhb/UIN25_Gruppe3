828529fc4e97bc5e2765e2ed21bd19866f905be5
Nils2312 <143592488+Nils2312@users.noreply.github.com>
2025-05-19 07:27:22 +0200

    La til innloggings funksjon på Dashboard.jsx

--- Billettlyst/src/components/Dashboard.jsx
+++ Billettlyst/src/components/Dashboard.jsx
@@ -1,16 +1,28 @@
+/*
+Kilde:
+https://react.dev/learn/sharing-state-between-components#reacting-to-input-with-state
+*/
+
+import { useState } from "react";
+
 export default function Dashboard() {
-    return (
-      <main className="dashboard">
-        <h1>Dashboard</h1>
-        <form className="loginform">
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  function handleLogin() {
+    setIsLoggedIn(true);
+  }  
+  return (
+    <main className="dashboard">
+      {isLoggedIn ? (
+        <h2>Min side</h2>
+      ) : (
+        <form className="loginform" onSubmit={handleLogin}>
           <h3>Logg inn</h3>
-  
           <label htmlFor="username">Brukernavn:</label>
           <input id="username" type="text" placeholder="brukernavn" />
-  
           <input type="submit" value="Logg inn" />
         </form>
-      </main>
-    );
-  }
-  
+      )}
+    </main>
+  );
+}
